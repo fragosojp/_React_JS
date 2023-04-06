3fdc6b6bf9f04b86de91c64c0784f65bae9b0528
Shystra <jlsp.2019@hotmail.com>
2023-04-06 19:32:12 -0300

    Mundando um titulo em 5s

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+class App extends Component {
+  state = {
+    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: 'O titulo 1',
+        body: 'O corpo 1',
+      },
+      {
+        id: 2,
+        title: 'O titulo 2',
+        body: 'O corpo 2'
+      },
+      {
+        id: 3,
+        title: 'O titulo 3',
+        body: 'O corpo 3'
+      },
+    ]
+  };
+
+componentDidMount () {
+  const {posts, counter} = this.state;
+  posts[0].title = 'O titulo mudou';
+
+  setTimeout(() => {
+    this.setState({ posts });
+  }, 5000);
+}
+
+render () {
+  const { posts, counter } = this.state;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <p>{counter}</p>
+      {posts.map(post =>(
+        <div key={post.id}>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
+        </div>
+      ))}
     </div>
   );
 }
-
+}
 export default App;
