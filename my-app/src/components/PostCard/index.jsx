628a738d42c7175c946a9caf823d62204fd5372a
Shystra <jlsp.2019@hotmail.com>
2023-04-06 23:10:13 -0300

    Props e Organização dos componentes

--- my-app/src/components/PostCard/index.jsx
+++ my-app/src/components/PostCard/index.jsx
@@ -1,10 +1,10 @@
-export const PostCard = ({title, cover, body, id}) => 
-    (
-        <div className='post'>
-            <img src={cover} alt={title} />
-            <div className='post-content'>
-                <h1>{title}</h1>
-                <p>{body}</p>
-            </div>
+export const PostCard = ({ title, cover, body, id }) =>
+(
+    <div className='post'>
+        <img src={cover} alt={title} />
+        <div className='post-content'>
+            <h1>{title}</h1>
+            <p>{body}</p>
         </div>
-    );
+    </div>
+);
