23b06642f087a21e30131b783d5f19edf4f168e9
Shystra <jlsp.2019@hotmail.com>
2023-04-06 21:24:04 -0300

    Subindo uma API posts

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,46 +5,25 @@ import { Component } from 'react';
 class App extends Component {
   state = {
     counter: 0,
-    posts: [
-      {
-        id: 1,
-        title: 'O titulo 1',
-        body: 'O corpo 1',
-      },
-      {
-        id: 2,
-        title: 'O titulo 2',
-        body: 'O corpo 2'
-      },
-      {
-        id: 3,
-        title: 'O titulo 3',
-        body: 'O corpo 3'
-      },
-    ]
+    posts: []
   };
-  timeoutUpdate = null;
+
 
 componentDidMount() {
-  this.handleTimeout();
+  this.loadPosts();
 }
 
-componentWillUnmount() {
-  clearTimeout(this.timeoutUpdate);
+loadPosts = async () => {
+  const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+
+  const [posts] = await Promise.all([postsResponse]);
+
+  const postsJason = await posts.json();
+
+  this.setState({ posts: postsJason });
 }
 
-componentDidUpdate() {
-  this.handleTimeout();
-}
 
-handleTimeout () {
-  const {posts, counter} = this.state;
-  posts[0].title = 'O titulo mudou';
-
-  this.timeoutUpdate = setTimeout(() => {
-    this.setState({ posts, counter: counter + 1 });
-  }, 1000);
-}
 
 render () {
   const { posts, counter } = this.state;
