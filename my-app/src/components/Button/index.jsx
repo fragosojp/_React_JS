4356dc62c3c9d93c66c0d19b17731050bbb9cb4f
Shystra <jlsp.2019@hotmail.com>
2023-04-07 12:35:56 -0300

    - Configuring Styles for page and customizing the Button "load more posts"

--- my-app/src/components/Button/index.jsx
+++ my-app/src/components/Button/index.jsx
@@ -1,11 +1,12 @@
+import './styles.css';
 import { Component } from "react";
 
 export class Button extends Component{
     render() {
-        const {text, onClick } = this.props;
+        const {text, onClick, disabled } = this.props;
 
         return (
-            <button onClick={onClick}>
+            <button disabled={disabled} className='button' onClick={onClick}>
                 {text}
             </button>
         );
