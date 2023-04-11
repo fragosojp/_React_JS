b54e45a64411284848d7ec56a7e5dd1b9b2112bb
Shystra <jlsp.2019@hotmail.com>
2023-04-10 21:32:54 -0300

    Ajustando pastas

--- my-app/src/Home/index.jsx
+++ my-app/src/Home/index.jsx
@@ -2,10 +2,10 @@
 import { Component } from 'react';
 import './styles.css';
 
-import { Posts } from '../../components/Posts';
-import { loadPosts } from '../../utils/load-posts';
-import { Button } from '../../components/Button/index.jsx';
-import { TextInput } from '../../components/TextInput/index.jsx';
+import { Posts } from '../components/Posts';
+import { loadPosts } from '../utils/load-posts';
+import { Button } from '../components/Button';
+import { TextInput } from '../components/TextInput';
 
 
 export class Home extends Component {
